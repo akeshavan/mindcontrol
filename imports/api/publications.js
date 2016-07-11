18c628718eb3528d177c85e19b742440774dbf97
Anisha Keshavan <akeshavan@ucla.edu>
2016-07-11 12:33:05 -0700

    danger: wipe whole db

--- imports/api/publications.js
+++ imports/api/publications.js
@@ -6,6 +6,7 @@ if (Meteor.isServer) {
   
   
  Meteor.startup(function () {
+    Subjects.remove({})
     if (Subjects.find().count() === 0) {
         //load the json from here: https://www.dropbox.com/s/enb5zemvmu2oqgw/data.json?dl=0
         var source_json = "https://dl.dropboxusercontent.com/s/vnohn5nh9ho3j8a/data_rf.json?dl=0"
