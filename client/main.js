ec31e5f7be117c5f351e6f98e0f00a40b10cceac
akeshavan <keshavan@berkeley.edu>
2016-07-13 15:48:05 -0700

    fix: flow router for separate page for viewer

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
+import "../imports/ui/routers.js"
 
 globalSelector = {}
 subjectSelector = {"subject_id": {"$in": []}}
