a82abf7b04242283ad7e8e41e1ca8669231afe8b
akeshavan <keshavan@berkeley.edu>
2016-07-26 10:28:39 -0700

    enh: ucsf specific changes for the development environment

--- imports/api/publications.js
+++ imports/api/publications.js
@@ -9,7 +9,7 @@ if (Meteor.isServer) {
     //Subjects.remove({})
     if (Subjects.find().count() === 0) {
         //load the json from here: https://www.dropbox.com/s/enb5zemvmu2oqgw/data.json?dl=0
-        var source_json = "https://dl.dropboxusercontent.com/s/vnohn5nh9ho3j8a/data_rf.json?dl=0"
+        var source_json = "http://localhost:3002/database/mindcontrol_initial_data.json"
         //console.log(HTTP.get(source_json).content)
         myobject = JSON.parse(HTTP.get(source_json).content)
         //console.log("my object is", myobject.length)
