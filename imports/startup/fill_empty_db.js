a7eaba7e224dba87eadc02f0f72f846dafe3ebf8
Anisha Keshavan <akeshavan@ucla.edu>
2016-09-07 11:57:26 -0700

    fix: can point to startup json from generator

--- imports/startup/fill_empty_db.js
+++ imports/startup/fill_empty_db.js
@@ -7,10 +7,13 @@ if (Meteor.isServer) {
   
  Meteor.startup(function () {
     //Subjects.remove({})
-    console.log("in metero startup function")
+    //console.log("in metero startup function")
     if (Subjects.find().count() === 0) {
         //load the json from here: https://www.dropbox.com/s/enb5zemvmu2oqgw/data.json?dl=0
-        var source_json = "https://dl.dropboxusercontent.com/s/vnohn5nh9ho3j8a/data_rf.json?dl=0"
+        //console.log(JSON.parse(HTTP.get("http:///private/generator.json").content))
+        myjson = JSON.parse(Assets.getText("generator.json"));
+        console.log("myjson", myjson["startup_json"])
+        var source_json = myjson["startup_json"] //"https://dl.dropboxusercontent.com/s/vnohn5nh9ho3j8a/data_rf.json?dl=0"
         //console.log(HTTP.get(source_json).content)
         myobject = JSON.parse(HTTP.get(source_json).content)
         //console.log("my object is", myobject.length)
