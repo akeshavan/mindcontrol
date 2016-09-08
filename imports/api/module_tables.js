1b58e222c08729cc13a441bbb19fd9d522f0da0d
Esha Datta <eshadatta@gmail.com>
2016-09-08 14:21:00 -0700

    corr branch generator

--- imports/api/module_tables.js
+++ imports/api/module_tables.js
@@ -2,7 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
-//export const Tasks = new Mongo.Collection('tasks');
 export const Subjects = new Mongo.Collection('subjects');
 
 import "./table_utils.js"
@@ -11,30 +10,50 @@ import "./methods.js"
 
 TabularTables = {}
 
-TabularTables.demographic =  new Tabular.Table({
-    name:"demographic",
-    //throttleRefresh: 1000,
-    autoWidth: false,
-    //selector: function(){return selector_function("demographic")},
-    collection: Subjects,
-    columns: [get_filter_field("demographic", "msid", "msid"),
-              get_filter_field("demographic", "subject_id", "Exam ID"),
-              get_filter_field("demographic", "Study Tag", "Study Tag"),
-              get_filter_field("demographic", "DCM_InstitutionName", "Site"),
-              get_filter_field("demographic", "metrics.DCM_StudyDate", "Date")]
-})
 
-TabularTables.freesurfer =  new Tabular.Table({
-    name:"freesurfer",
+
+TabularTables.longitudinal_quality_metrics =  new Tabular.Table({
+name:"longitudinal_quality_metrics",
+    autoWidth: false,
     collection: Subjects,
-    //selector: function(){return selector_function("demographic")},
-    autoWidth: true,
-    columns: [get_filter_field("freesurfer", "subject_id", "Exam ID"),
-              get_qc_viewer("freesurfer", "name", "Freesurfer ID"), //tableFields["viewFS"],
-              get_qc_filter_field("freesurfer", "quality_check.QC", "QC"), //tableFields["QC"],
-              get_filter_field("freesurfer", "checkedBy", "checked by"),
-              get_filter_list_field("freesurfer", "quality_check.user_assign", "Assigned To"),
-              {data:"quality_check.notes_QC", title:"Notes"}
-              //tableFields["assignedTo"]
-              ]
-})
+    columns:[
+    
+      
+        get_qc_viewer("longitudinal_quality_metrics", "name", "Name"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "msid", "Subject ID"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "subject_id", "Exam ID"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "site", "Site"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "session", "Session"),
+      
+    
+      
+        get_qc_filter_field("longitudinal_quality_metrics", "quality_check.QC", "QC"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "checkedBy", "checked by"),
+      
+    
+      
+        get_filter_field("longitudinal_quality_metrics", "quality_check.user_assign", "Assigned To"),
+      
+    
+      
+        {data: "quality_check.notes_QC", title: "Notes" }
+      
+    
+    ]
+    })
+
