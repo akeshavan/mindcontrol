76d39b937e325041ebf011efa751da8d1e4051b6
Anisha Keshavan <akeshavan@ucla.edu>
2016-11-23 15:34:41 -0800

    wip: parallel coords

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,7 +1,7 @@
 import {Subjects} from "./module_tables.js"
 
 Meteor.methods({
-    
+
     getDateHist: function(){
             //console.log("running the aggregate")
             if (Meteor.isServer){
@@ -10,9 +10,9 @@ Meteor.methods({
                 return foo
             }
 
-            
+
       },
-      
+
     getHistogramData: function(entry_type, metric, bins, filter){
           //console.log("getting histogram data")
           if (Meteor.isServer){
@@ -21,28 +21,28 @@ Meteor.methods({
           var metric_name = "metrics."+metric
           //no_null["metrics"] = {}
           //no_null["metrics"]["$ne"] = null
-          
+
           if (Object.keys(no_null).indexOf(metric_name) >=0 ){
               no_null[metric_name]["$ne"] = null
           }
           else{
               no_null[metric_name] = {$ne: null}
           }
-          
+
           //console.log("in the server, the filter is", no_null)
-          
+
           var minval = Subjects.find(no_null, {sort: [[metric_name, "ascending"]], limit: 1}).fetch()[0]["metrics"][metric]
           var maxval = Subjects.find(no_null, {sort: [[metric_name, "descending"]], limit: 1}).fetch()[0]["metrics"][metric]
                     //var minval = Subjects.findOne({"entry_type": entry_type, no_null}, {sort: minsorter})//.sort(maxsorter).limit(1)
-          
-          
+
+
           var bin_size = (maxval -minval)/(bins+1)
           console.log("the bin size is", bin_size)
-          
+
           if (bin_size){
-                var foo = Subjects.aggregate([{$match: no_null}, 
-                    {$project: {lowerBound: {$subtract: ["$metrics."+metric, 
-                        {$mod: ["$metrics."+metric, bin_size]}]}}}, 
+                var foo = Subjects.aggregate([{$match: no_null},
+                    {$project: {lowerBound: {$subtract: ["$metrics."+metric,
+                        {$mod: ["$metrics."+metric, bin_size]}]}}},
                     {$group: {_id: "$lowerBound", count: {$sum: 1}}}])
                 var output = {}
                 output["histogram"] = _.sortBy(foo, "_id")
@@ -59,10 +59,36 @@ Meteor.methods({
           }}
           //{entry_type: "freesurfer"}
 
-          
-                            
+
+
       },
-    
+
+    getAllData: function(entry_type, metrics, bins, filter){
+      if (Meteor.isServer){
+        var no_null = filter
+        no_null["entry_type"] = entry_type
+
+        metrics.forEach(function(metric, idx, arr){
+          var metric_name = "metrics."+metric
+          //no_null["metrics"] = {}
+          //no_null["metrics"]["$ne"] = null
+
+          if (Object.keys(no_null).indexOf(metric_name) >=0 ){
+              no_null[metric_name]["$ne"] = null
+          }
+          else{
+              no_null[metric_name] = {$ne: null}
+          }
+
+        })
+
+        console.log("in getAllData", no_null)
+        var data = Subjects.find(no_null).fetch()
+        var output = _.map(data, function(d){return d.metrics})
+        return {data: output}
+      }
+    },
+
     get_subject_ids_from_filter: function(filter){
         if (Meteor.isServer){
             var subids = []
@@ -72,16 +98,16 @@ Meteor.methods({
             //console.log("the number subjects to filter by are",filter, subids.length)
             return subids
         }
-        
+
     },
-    
+
     updateQC: function(qc, form_data){
         //console.log(form_data)
         Subjects.update({entry_type: qc.entry_type, name:qc.name}, {$set: form_data})
     },
-    
+
     get_metric_names: function(entry_type){
-        
+
         if (Meteor.isServer){
             no_null= {metrics: {$ne: {}}, "entry_type": entry_type}
             var dude = Subjects.findOne(no_null)
@@ -89,22 +115,22 @@ Meteor.methods({
                 return Object.keys(dude["metrics"])
             }
             //console.log("dude is", dude)
-            
+
         }
-        
+
     },
-    
+
     save_query: function(name, gSelector){
         var topush = {"name": name, "selector": gSelector}
         Meteor.users.update(this.userId, {$push: {queries: topush}})
     },
-    
+
     removeQuery: function(query, name){
 
         console.log("query is", query, name)
         var topull = {"name": name, "selector": query}
         Meteor.users.update(this.userId, {$pull: {queries: topull}})
-        
+
     },
 
     get_generator: function(entry_type){
@@ -122,5 +148,5 @@ Meteor.methods({
         return output
         }}
     }
-    
+
   });
--- imports/ui/module_templates.js
+++ imports/ui/module_templates.js
@@ -4,6 +4,31 @@ import { Template } from 'meteor/templating';
 import './module_templates.html';
 import "./d3_plots.js"
 
+window.pcz = {}
+
+var zcolorscale = d3.scale.linear()
+  .domain([-2,-0.5,0.5,2])
+  .range(["brown", "#999", "#999", "steelblue"])
+  .interpolate(d3.interpolateLab);
+
+  // return color function based on plot and dimension
+
+  // return color function based on plot and dimension
+  function zcolor(col, dimension) {
+    var z = zscore(_(col).pluck(dimension).map(parseFloat))
+    return function(d) { return zcolorscale(z(d[dimension])) }
+  };
+
+  // color by zscore
+  function zscore(col) {
+    var n = col.length,
+        mean = _(col).mean(),
+        sigma = _(col).stdDeviation();
+    return function(d) {
+      return (d-mean)/sigma;
+    };
+  };
+
 
 get_filter = function(entry_type){
 
@@ -75,7 +100,119 @@ render_histogram = function(entry_type){
                 }
 }
 
+render_parcoor = function(entry_type){
+  var metric = Session.get("current_"+entry_type)//"Amygdala"
+  if (metric == null){
+      var all_metrics = Session.get(entry_type+"_metrics")
 
+      /*if (all_metrics != null){
+          Session.set("current_"+entry_type, all_metrics)
+      }*/
+  }
+
+  var metrics = Session.get(entry_type+"_metrics")
+
+
+  if (metrics != null){
+      var filter = get_filter(entry_type)
+      //console.log("filter is", filter)
+      Meteor.call("getAllData", entry_type, metrics, 20, filter, function(error, result){
+      //console.log("result is", result)
+      var data = result["data"]
+
+      if (data.length){
+        //$(".parcoords_"+entry_type).height("200px");
+          window.pcz[entry_type] = window.pcz[entry_type] || d3.parcoords()("#parcoords_"+entry_type)
+          var pcz = window.pcz[entry_type]
+          pcz.data(data)
+                  .bundlingStrength(1) // set bundling strength
+                  .smoothness(0)
+                  .alphaOnBrushed(.1)
+                  //.bundleDimension("cylinders")
+                  .showControlPoints(false)
+                  //.hideAxis(me.hideKeys)
+                  .render()
+                  .brushMode("1D-axes")
+                  .reorderable()
+                  .interactive();
+          pcz.brushReset()
+          var change_color = function(dimension) {
+                pcz.svg.selectAll(".dimension")
+                  .style("font-weight", "normal")
+                  .filter(function(d) { return d == dimension; })
+                  .style("font-weight", "bold")
+                console.log("dimension", dimension)
+                pcz.color(zcolor(pcz.data(),dimension)).render()
+              }
+
+          //change_color(metrics[0]);
+          pcz.svg.selectAll(".dimension")
+                  .on("click", change_color)
+                  .selectAll(".label")
+                  .style("font-size", "14px");
+
+
+          pcz.on("brushend", function(){
+            console.log(pcz.brushExtents())
+
+            var metrics = pcz.brushExtents()
+            $.each(metrics, function(metric, value){
+                var newkey = "metrics."+metric
+                var gSelector = Session.get("globalSelector")
+                if (Object.keys(gSelector).indexOf(entry_type) < 0 ){
+                    gSelector[entry_type] = {}
+                }
+                gSelector[entry_type][newkey] = {$gte: value[0], $lte: value[1]}
+                Session.set("globalSelector", gSelector)
+
+                var filter = get_filter(entry_type)
+                filter[newkey] = {$gte: value[0], $lte: value[1]}
+
+                Meteor.call("get_subject_ids_from_filter", filter, function(error, result){
+                    //console.log("result from get subject ids from filter is", result)
+                    var ss = Session.get("subjectSelector")
+                    ss["subject_id"]["$in"] = result
+                    Session.set("subjectSelector", ss)
+                })
+
+            })
+
+            /*var newkey = "metrics."+metric
+
+
+            var gSelector = Session.get("globalSelector")
+            if (Object.keys(gSelector).indexOf(entry_type) < 0 ){
+                gSelector[entry_type] = {}
+            }
+            gSelector[entry_type][newkey] = {$gte: extent0[0], $lte: extent0[1]}
+            Session.set("globalSelector", gSelector)
+
+            var filter = get_filter(entry_type)
+            filter[newkey] = {$gte: extent0[0], $lte: extent0[1]}
+
+            Meteor.call("get_subject_ids_from_filter", filter, function(error, result){
+                //console.log("result from get subject ids from filter is", result)
+                var ss = Session.get("subjectSelector")
+                ss["subject_id"]["$in"] = result
+                Session.set("subjectSelector", ss)
+            })*/
+
+
+
+
+
+          })
+          //$("#search_metric_"+entry_type).select2({data: metrics, theme:"bootstrap"});
+
+      }
+      else{
+          console.log("empty parcoords data")
+      }
+
+
+      });
+  }
+}
 
 Template.demographic.rendered = function() {
 
@@ -84,14 +221,14 @@ Template.demographic.rendered = function() {
          }
 
 
-    
+
 
        this.autorun(function() {
                 render_histogram("demographic")
 
        }); //end autorun
 
-     
+
 
   }
 
@@ -106,14 +243,14 @@ Template.qa.rendered = function() {
          }
 
 
-    
+
 
        this.autorun(function() {
                 render_histogram("qa")
-
+                render_parcoor("qa")
        }); //end autorun
 
-     
+
 
   }
 
@@ -128,14 +265,14 @@ Template.ants.rendered = function() {
          }
 
 
-    
+
 
        this.autorun(function() {
                 render_histogram("ants")
 
        }); //end autorun
 
-     
+
 
   }
 
@@ -203,7 +340,7 @@ currentMetric: function(){
 
 
 
-  
+
    Template.demographic.events({
     "change #metric-select-demographic": function(event, template){
         var metric = $(event.currentTarget).val()
@@ -211,9 +348,9 @@ currentMetric: function(){
         Session.set("current_demographic", metric)
     }
    })
-  
 
-  
+
+
    Template.qa.events({
     "change #metric-select-qa": function(event, template){
         var metric = $(event.currentTarget).val()
@@ -221,9 +358,9 @@ currentMetric: function(){
         Session.set("current_qa", metric)
     }
    })
-  
 
-  
+
+
    Template.ants.events({
     "change #metric-select-ants": function(event, template){
         var metric = $(event.currentTarget).val()
@@ -231,4 +368,3 @@ currentMetric: function(){
         Session.set("current_ants", metric)
     }
    })
-  
