139df1685e77aed1f7a243e68b09dad8e54126ce
akeshavan <keshavan@berkeley.edu>
2017-06-23 12:08:39 -0700

    enh: removing python generation part

--- imports/api/module_tables.js
+++ imports/api/module_tables.js
@@ -10,34 +10,58 @@ import "./methods.js"
 
 TabularTables = {}
 
+console.log("Meteor settings are", Meteor.settings)
 
+Meteor.settings.public.modules.forEach(function(mod, idx, arr){
+  TabularTables[mod.entry_type] = new Tabular.Table({
+    name: mod.entry_type,
+    collection: Subjects,
+    autoWidth: false,
+    columns: _.map(mod.fields, function(field){
+      switch (field.function_name) {
+        case "get_filter_field":
+          return get_filter_field(mod.entry_type, field.id, field.name)
+          break;
+        case "get_qc_viewer":
+          return get_qc_viewer(mod.entry_type, field.id, field.name)
+          break;
+        case "get_qc_filter_field":
+          return get_qc_filter_field(mod.entry_type, field.id, field.name)
+        default:
+          return {data: field.id, title: field.name}
 
+      }
+    })
+  })
+})
+
+/*
 TabularTables.demographic =  new Tabular.Table({
 name:"demographic",
     autoWidth: false,
     collection: Subjects,
     columns:[
-    
-      
+
+
         get_filter_field("demographic", "msid", "msid"),
-      
-    
-      
+
+
+
         get_filter_field("demographic", "subject_id", "Exam ID"),
-      
-    
-      
+
+
+
         get_filter_field("demographic", "Study Tag", "Study Tag"),
-      
-    
-      
+
+
+
         get_filter_field("demographic", "DCM_InstitutionName", "Site"),
-      
-    
-      
+
+
+
         get_filter_field("demographic", "metrics.DCM_InstitutionName", "Date"),
-      
-    
+
+
     ]
     })
 
@@ -47,31 +71,31 @@ name:"freesurfer",
     autoWidth: false,
     collection: Subjects,
     columns:[
-    
-      
+
+
         get_filter_field("freesurfer", "subject_id", "Exam ID"),
-      
-    
-      
+
+
+
         get_qc_viewer("freesurfer", "name", "Freesurfer ID"),
-      
-    
-      
+
+
+
         get_qc_filter_field("freesurfer", "quality_check.QC", "QC"),
-      
-    
-      
+
+
+
         get_filter_field("freesurfer", "checkedBy", "checked by"),
-      
-    
-      
+
+
+
         get_filter_field("freesurfer", "quality_check.user_assign", "Assigned To"),
-      
-    
-      
+
+
+
         {data: "quality_check.notes_QC", title: "Notes" }
-      
-    
+
+
     ]
     })
 
@@ -81,31 +105,31 @@ name:"test",
     autoWidth: false,
     collection: Subjects,
     columns:[
-    
-      
+
+
         get_filter_field("test", "subject_id", "Exam ID"),
-      
-    
-      
+
+
+
         get_qc_viewer("test", "name", "View Image"),
-      
-    
-      
+
+
+
         get_qc_filter_field("test", "quality_check.QC", "QC"),
-      
-    
-      
+
+
+
         get_filter_field("test", "checkedBy", "checked by"),
-      
-    
-      
+
+
+
         get_filter_field("test", "quality_check.user_assign", "Assigned To"),
-      
-    
-      
+
+
+
         {data: "quality_check.notes_QC", title: "Notes" }
-      
-    
+
+
     ]
     })
-
+*/
