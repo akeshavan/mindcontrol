2f9478b88be5e56a559044d9cf9258c923867f02
Lee Tirrell <ltirrell@corticometrics.com>
2017-06-23 20:20:09 -0700

    fixed path... again

--- imports/docker/entrypoint.sh
+++ imports/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /bids/derivatives/mindcontrol_freesurfer/
-python3 /mindcontrol/imports/python_generate/start_static_server &
+python3 /home/mindcontrol/mindcontrol/imports/python_generate/start_static_server &
 
 cd /home/mindcontrol/mindcontrol
 meteor --settings settings_freesurfer.json
