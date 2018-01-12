ff2b75d3d1ef2cf76391b89ae5c1c73b2b84095e
akeshavan <keshavan@berkeley.edu>
2018-01-11 16:47:05 -0800

    fix: sticky sidebar a little better on scroll

--- imports/ui/custom.js
+++ imports/ui/custom.js
@@ -64,16 +64,19 @@ Template.consent.events({
 
 Template.consent.rendered = function(){
   window.onscroll = function() {myFunction()};
-
-
+  var navbar = document.getElementById("sticky");
+  if (navbar){
+    var sticky = navbar.offsetTop;
+  } else {
+    var sticky = null;
+  }
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function myFunction() {
     // Get the navbar
-    var navbar = document.getElementById("sticky");
 
-    if (navbar){
+
+    if (navbar && sticky){
       // Get the offset position of the navbar
-      var sticky = navbar.offsetTop;
 
       if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
